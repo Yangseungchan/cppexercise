f6656d95ff1873d5014b4682511c45d61de2ba3b
Yangseungchan <uvzone@naver.com>
2020-08-30 15:42:53 +0900

    0830 Update

---
+++ ExceptionHandling/BadAlloc.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <new>
+
+using namespace std;
+
+int main(void)
+{
+    int num = 0;
+
+    try
+    {
+        while (1)
+        {
+            num++;
+            cout << num << "번째 할당 시도" << endl;
+            new int[1000000][1000000];
+        }
+    }
+    catch (bad_alloc &bad)
+    {
+        cout << bad.what() << endl;
+        cout << "더 이상 할당 불가!" << endl;
+    }
+
+    return 0;
+}
+
